926268e6d6dd8b19554cafdf3cd926bfcdd2e8d5
Bilal Yasin <byasin@tier2consulting.com>
2021-08-03 13:32:29 +0100

    Added Enumeration to Difficulty property in Recipe.

---
+++ src/main/java/com/springframework/recipeapp/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package com.springframework.recipeapp.domain;
+
+public enum Difficulty {
+    EASY, MODERATE, HARD
+}
--- src/main/java/com/springframework/recipeapp/domain/Recipe.java
+++ src/main/java/com/springframework/recipeapp/domain/Recipe.java
@@ -16,7 +16,9 @@ public class Recipe {
     private Integer servings;
     private String source;
     private String url;
-//    private Difficulty difficulty;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
@@ -81,6 +83,22 @@ public class Recipe {
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 
     public Byte[] getImages() {
