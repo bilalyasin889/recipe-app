1d8894b28f8d481f6422492995b83122d995f25d
Bilal Yasin <byasin@tier2consulting.com>
2021-08-04 13:18:46 +0100

    Added Bootstrap css and Added Junit Test for Category and RecipeService.

---
+++ src/test/java/com/springframework/recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,52 @@
+package com.springframework.recipeapp.services;
+
+import com.springframework.recipeapp.domain.Recipe;
+import com.springframework.recipeapp.repositories.RecipeRepository;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+    AutoCloseable closeable;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        closeable = MockitoAnnotations.openMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        closeable.close();
+    }
+
+    @Test
+    public void getRecipes() {
+
+        Recipe recipe = new Recipe();
+        HashSet recipeData = new HashSet();
+        recipeData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipeData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(1, recipes.size());
+        verify(recipeRepository, times(1)).findAll();
+
+    }
+}
