6dce5eb9cbdbe819ada8a4518409462405b63d2c
Bilal Yasin <byasin@tier2consulting.com>
2021-08-04 14:46:20 +0100

    Updated Junit Test for IndexController: Added MockMVC Test.

--- src/test/java/com/springframework/recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/com/springframework/recipeapp/controllers/IndexControllerTest.java
@@ -8,6 +8,9 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -15,6 +18,9 @@ import java.util.Set;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class IndexControllerTest {
 
@@ -35,6 +41,15 @@ public class IndexControllerTest {
     @After
     public void tearDown() throws Exception {
         closeable.close();
+    }
+
+    @Test
+    public void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
     }
 
     @Test
