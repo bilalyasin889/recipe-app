6087636aaeae9c34ed54b2305b4bd479f15c21d3
Bilal Yasin <byasin@tier2consulting.com>
2021-08-04 14:54:56 +0100

    Added Junit Integration Test for UnitOfMeasureRepository.

---
+++ src/test/java/com/springframework/recipeapp/repositories/UnitOfMeasureRepositoryTestIT.java
@@ -0,0 +1,29 @@
+package com.springframework.recipeapp.repositories;
+
+import com.springframework.recipeapp.domain.UnitOfMeasure;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryTestIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Test
+    public void findByDescription() {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", uomOptional.get().getDescription());
+    }
+}
