bf078b440468e79bd676245ad0b9c9753e018e91
Bilal Yasin <byasin@tier2consulting.com>
2021-08-05 10:22:34 +0100

    Added Show Recipe page and Updated index.html. Added data to RecipeBootstrap. Added Test for RecipeServiceImpl and RecipeController

---
+++ src/test/java/com/springframework/recipeapp/controllers/RecipeControllerTest.java
@@ -0,0 +1,54 @@
+package com.springframework.recipeapp.controllers;
+
+import com.springframework.recipeapp.domain.Recipe;
+import com.springframework.recipeapp.services.RecipeService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class RecipeControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    RecipeController controller;
+
+    AutoCloseable closeable;
+
+    @Before
+    public void setUp() throws Exception {
+        closeable = MockitoAnnotations.openMocks(this);
+
+        controller = new RecipeController(recipeService);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        closeable.close();
+    }
+
+    @Test
+    public void testGetRecipe() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        when(recipeService.findById(anyLong())).thenReturn(recipe);
+
+        mockMvc.perform(get("/recipe/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"))
+                .andExpect(model().attributeExists("recipe"));;
+    }
+}
